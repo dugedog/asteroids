4fe55700f1cdaa9c7f72bbdb4a066f659001a9a7
dugedog <dugedog@gmail.com>
2025-09-22 21:18:51 +1000

    CH2.L7: Moving - Complete

--- main.py
+++ main.py
@@ -4,21 +4,31 @@
 
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    ## I am manually definiung the radius in the following line, i would not be
+    player_no_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    
     while True:
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
                 return
+        
+        player_no_1.update(dt)
+
         screen.fill(000)
+        player_no_1.draw(screen)
         pygame.display.flip()
 
+        dt =  game_clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
